20937daf63e54070749832d2ae53a12078a1ca2c
yasith uthpala <167942712+yasith-uthpala@users.noreply.github.com>
2025-11-14 12:32:19 +0530

    Length Update
    Minimum length is 8(for strong password)

--- pass_gen.py
+++ pass_gen.py
@@ -9,7 +9,9 @@ length = int(input("Enter password length: "))
 password = ""
 
 # Generate password character by character
-for i in range(length):
-    password += random.choice(characters)
-
-print("Generated Password:", password)
+if length < 8:
+    print("Password length should be at least 8 characters.")
+else:
+    for i in range(length):
+        password += random.choice(characters)
+    print("Generated Password:", password)
